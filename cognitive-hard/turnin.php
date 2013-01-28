7a465dad9643f680b8081af409e41c3676da05d1
calebqian <calebqian.uiuc@gmail.com>
2013-01-28 17:52:19 -0600

    release 1.0 cognitive-hard updated

---
+++ cognitive-hard/turnin.php
@@ -0,0 +1,57 @@
+<meta charset="UTF-8">
+<?php
+    $host = "us-cdbr-azure-west-b.cleardb.com";
+    $user = "b8f4d17ca45332";
+    $pwd = "fb083c22";
+    $db = "gamestiAiNKRp4I2";
+//echo "Helllo world from PHP!";
+  try {
+        $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } 
+    catch(Exception $e){
+        die(var_dump($e));
+    }
+	
+	//echo "so far so good";
+	
+	if(!empty($_POST)){
+		try{
+		
+		//	echo "reached here 1";
+			$gametype= $_POST['gametype'];
+			//echo "reached here 2";
+			$pairs = $_POST['Pairs'];
+				//		echo "reached here 3";
+			$total = $_POST['Total'];
+			//	echo "reached here 4";
+			$percent = $_POST['Percent'];
+					//	echo "reached here 5";
+			$havead = intval($_POST['HaveAd']);
+					//	echo "reached here 6";
+			$sql_insert = "INSERT INTO survey (GameType, Pairs, Total, Percent, HaveAd) VALUES (?, ?, ?, ?, ?)";
+			
+			//echo "sql_insert sentence created.";
+			//echo $gametype;
+			//echo $pairs;
+			//echo $total;
+			//echo $havead;
+				
+			 $stmt = $conn->prepare($sql_insert);
+			$stmt->bindValue(1, $gametype);
+			$stmt->bindValue(2, $pairs);
+			$stmt->bindValue(3, $total);
+			$stmt->bindValue(4, $percent);
+			$stmt->bindValue(5, $havead);
+			$stmt->execute();
+		
+		}
+	   catch(Exception $e) {
+        die(var_dump($e));
+		}
+	
+	}
+	
+	
+// some code
+?>
