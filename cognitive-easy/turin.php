b0e91867471c506f6618daa086f62686830dbdf1
calebqian <calebqian.uiuc@gmail.com>
2013-01-28 15:23:28 -0600

    php connection test azure php

--- cognitive-easy/turin.php
+++ cognitive-easy/turin.php
@@ -1,14 +1,21 @@
 <meta charset="UTF-8">
 <?php
-//echo "Helllo world from PHP!"; 
-$con = mysql_connect("us-cdbr-azure-west-b.cleardb.com","b8f4d17ca45332","fb083c22");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
-mysql_select_db("gamestiAiNKRp4I2", $con);
+    $host = "us-cdbr-azure-west-b.cleardb.com";
+    $user = "b8f4d17ca45332";
+    $pwd = "fb083c22";
+    $db = "gamestiAiNKRp4I2";
+//echo "Helllo world from PHP!";
+  try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } 
+    catch(Exception $e){
+        die(var_dump($e));
+    }
 
-mysql_query("INSERT INTO survey (GameType, Pairs, Total, Percent, HaveAd) VALUES ('cognitive-easy', 4, 8, 0.567898093, TRUE)");
+$sql_insert = "INSERT INTO survey (GameType, Pairs, Total, Percent, HaveAd) VALUES ('cognitive-easy', 4, 8, 0.567898093, TRUE)";
+
+$stmt = $conn->query($sql_select);
 
 
 // some code
