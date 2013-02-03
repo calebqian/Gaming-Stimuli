d8cacf07ecaebf510dfa38a75ad4a2864e3f6075
calebqian <calebqian.uiuc@gmail.com>
2013-02-03 17:51:32 -0600

    admin portal complete fi

--- cognitive-easy/turnin.php
+++ cognitive-easy/turnin.php
@@ -29,7 +29,7 @@
 					//	echo "reached here 5";
 			$havead = intval($_POST['HaveAd']);
 					//	echo "reached here 6";
-			$sql_insert = "INSERT INTO survey (GameType, Pairs, Total, Percent, HaveAd, submitdate) VALUES (?, ?, ?, ?, ?, ?)";
+			$sql_insert = "INSERT INTO survey (GameType, Pairs, Total, Percent, HaveAd, submitdate, IP) VALUES (?, ?, ?, ?, ?, ?,?)";
 			
 			//echo "sql_insert sentence created.";
 			//echo $gametype;
@@ -37,7 +37,8 @@
 			//echo $total;
 			//echo $havead;
 			date_default_timezone_set('America/Chicago');
-				$current_time = date('Y-m-d H:i:s');
+			$ip=$_SERVER['REMOTE_ADDR'];
+			$current_time = date('Y-m-d H:i:s');
 			 $stmt = $conn->prepare($sql_insert);
 			$stmt->bindValue(1, $gametype);
 			$stmt->bindValue(2, $pairs);
@@ -45,6 +46,7 @@
 			$stmt->bindValue(4, $percent);
 			$stmt->bindValue(5, $havead);
 			$stmt->bindValue(6, $current_time);
+			$stmt->bindValue(7, $ip);
 			$stmt->execute();
 		
 		}
