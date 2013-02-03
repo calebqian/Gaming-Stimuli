d9d59d7ca246bca58d10d744d8f25dfc13b3d3a9
calebqian <calebqian.uiuc@gmail.com>
2013-02-02 20:48:56 -0600

    admin portal half

--- admin/index.php
+++ admin/index.php
@@ -1,32 +1,67 @@
+<html>
+<head>
+<Title>Administration Portal</Title>
+
+</head>
+<body>
+
+
+<form method="post" action="truncate.php" enctype="multipart/form-data" >
+ <input type = 'submit' value = 'Clear All Data'/>  
+</form>
+<form method = "post" action = "export.php" enctype="multipart/form-data"> 
+<input type = 'submit' value = 'Export'/> 
+</form>
 <?php
-$username="b8f4d17ca45332";
-$password="fb083c22";
-$database="gamestiAiNKRp4I2";
-
-mysql_connect("us-cdbr-azure-west-b.cleardb.com",$username,$password);
-@mysql_select_db($database) or die( "Unable to select database");
-$query="SELECT * FROM survey";
-$result=mysql_query($query);
-
-$num=mysql_numrows($result);
-
-mysql_close();
-
-echo "<b><center>Database Output</center></b><br><br>";
-
-$i=0;
-while ($i < $num) {
-
-$field1-name=mysql_result($result,$i,"field1-name");
-$field2-name=mysql_result($result,$i,"field2-name");
-$field3-name=mysql_result($result,$i,"field3-name");
-$field4-name=mysql_result($result,$i,"field4-name");
-$field5-name=mysql_result($result,$i,"field5-name");
-
-echo "<b>$field1-name 
-$field2-name2</b><br>$field3-name<br>$field4-name<br>$field5-name<hr><br>";
-
-$i++;
+    // DB connection info
+    //TODO: Update the values for $host, $user, $pwd, and $db
+    //using the values you retrieved earlier from the portal.
+    $host = "us-cdbr-azure-west-b.cleardb.com";
+    $user = "b8f4d17ca45332";
+    $pwd = "fb083c22";
+    $db = "gamestiAiNKRp4I2";
+    // Connect to database.
+    try {
+        $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch(Exception $e){
+        die(var_dump($e));
+    }
+    // Insert registration info
+	//echo "good here";
+	
+	$sql_request = "SELECT * FROM survey";
+	$stmt = $conn->query($sql_request);
+	//$result = $stmt->fetchAll();
+	//$num =  count($result);
+	$arrValues = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// open the table
+print "<table wdith=\"100%\">\n";
+print "<tr>\n";
+// add the table headers
+foreach ($arrValues[0] as $key => $useless){
+    print "<th>$key</th>";
 }
+print "</tr>";
+// display data
+foreach ($arrValues as $row){
+    print "<tr>";
+    foreach ($row as $key => $val){
+        print "<td align='center'>$val</td>";
+    }
+	print "<td><input type='button' value='delete'/></td>";
+    print "</tr>\n";
+}
+// close the table
+print "</table>\n";
+	
+	
+	
+	//echo $num;
+	//echo "good here";
 
+	
 ?>
+</body>
+</html>
