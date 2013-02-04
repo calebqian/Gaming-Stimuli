d2760aeac511680e2bd211142c7937b7f41ea224
calebqian <calebqian.uiuc@gmail.com>
2013-02-04 15:59:51 -0600

    hello geta 11

--- game-easy/geta.php
+++ game-easy/geta.php
@@ -22,9 +22,9 @@
 		echo "good after\n";
 	$result = $stmt->fetch();
 	echo "good after 2\n";
-	$sql_request = "UPDATE `subj` SET `subNum` = `subNum`+1 WHERE `ID` = 1";
+	$sql_request = "UPDATE subj SET subNum = subNum + ? WHERE ID = ?";
 	$stmt = $conn->prepare($sql_request);
-	$stmt = $conn->execute();
+	$stmt = $conn->execute(array(1, 1));
 	echo "good after 3\n";
 	echo $result['subNum']+1;
 	echo "good after 4\n";
