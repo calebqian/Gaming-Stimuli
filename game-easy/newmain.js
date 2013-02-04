fccfe281ea29280bc9bdd0d63c7b1a3309ac0d0e
calebqian <calebqian.uiuc@gmail.com>
2013-02-04 15:21:32 -0600

    hello geta 2

--- game-easy/newmain.js
+++ game-easy/newmain.js
@@ -1651,6 +1651,7 @@ contDownHandler = function(evt) {
             url: frm.attr('action'),
             data: frm.serialize(),
             success: function (data) {
+			alert("mark assigned");
              alert(data);
             }
         });
