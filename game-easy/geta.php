2fdf202d2809f5d7d002f5a8c254dbeefbc02f82
calebqian <calebqian.uiuc@gmail.com>
2013-02-04 15:38:07 -0600

    hello geta 5

--- game-easy/geta.php
+++ game-easy/geta.php
@@ -14,22 +14,20 @@
         die(var_dump($e));
     }
 	
-	$sql_request = "START TRANSACTION; SELECT subNum FROM subj WHERE ID = 1 FOR UPDATE; UPDATE subj SET subNum = subNum+1 WHERE ID = 1;";
+	$sql_request = "START TRANSACTION";
+	$stmt = $conn->query($sql_request);
+	$sql_request = "SELECT subNum FROM subj WHERE ID = 1 FOR UPDATE";
+	$stmt = $conn->query($sql_request);
+	$result = $stmt->fetch();
+	$sql_request = "UPDATE subj SET subNum = subNum+1 WHERE ID = 1";
 	$stmt = $conn->prepare($sql_request);
-	//$sql_request = "SELECT subNum FROM subj WHERE ID = 1 FOR UPDATE";
-	//$stmt = $conn->query($sql_request);
-	//$result = $stmt->fetch();
-	//$sql_request = "UPDATE subj SET subNum = subNum+1 WHERE ID = 1";
-	//$stmt = $conn->prepare($sql_request);
-	//$stmt = $conn->execute();
-	//$subnum = $result['subNum']+1;
-	//echo $subnum;
 	$stmt = $conn->execute();
+	$subnum = $result['subNum']+1;
+	echo $subnum;
 	
 	if($stmt){
 	  $sql_request = "COMMIT";
 	  $stmt = $conn->query($sql_request);
-	  
 	 }
 	else{
 		$sql_request = "ROLLBACK";
